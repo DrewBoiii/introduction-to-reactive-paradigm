f69b142c27cdcd84fc92c69f12f6aa15ab9f73f7
drewboiii <goodchickenboy@mail.ru>
2022-01-19 13:42:19 +0400

    section 13

--- src/main/java/com/learnreactiveprogramming/service/MovieReactiveService.java
+++ src/main/java/com/learnreactiveprogramming/service/MovieReactiveService.java
@@ -3,16 +3,10 @@ package com.learnreactiveprogramming.service;
 import com.learnreactiveprogramming.domain.Movie;
 import com.learnreactiveprogramming.domain.MovieInfo;
 import com.learnreactiveprogramming.exception.MovieException;
-import com.learnreactiveprogramming.exception.NetworkException;
-import com.learnreactiveprogramming.exception.ServiceException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.util.retry.Retry;
-
-import java.time.Duration;
 
 @Slf4j
 @AllArgsConstructor
@@ -26,57 +20,6 @@ public class MovieReactiveService {
                 .flatMap(this::getMovie)
                 .doOnError(throwable -> log.error("Movie error", throwable))
                 .onErrorMap(MovieException::new)
-                .retry(5L)
-                .log();
-    }
-
-    public Flux<Movie> getAllMoviesRetryWhen() {
-        return movieInfoService.retrieveMoviesFlux()
-                .flatMap(this::getMovie)
-                .doOnError(throwable -> log.error("Movie error", throwable))
-                .onErrorMap(throwable -> {
-                    if (throwable instanceof NetworkException) {
-                        throw new MovieException(throwable);
-                    }
-                    throw new ServiceException(throwable);
-                })
-                .retryWhen(Retry.backoff(5L, Duration.ofMillis(100L))
-                        .filter(throwable -> throwable instanceof MovieException) // use this if you want to specify which of the exception we want to propagate
-                        .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> Exceptions.propagate(retrySignal.failure()))) // if we don't do this, we get Exhausted Exception instead of Movie exception or any specified exceptions
-                .log();
-    }
-
-    public Flux<Movie> getAllMoviesRepeat() {
-        return movieInfoService.retrieveMoviesFlux()
-                .flatMap(this::getMovie)
-                .doOnError(throwable -> log.error("Movie error", throwable))
-                .onErrorMap(throwable -> {
-                    if (throwable instanceof NetworkException) {
-                        throw new MovieException(throwable);
-                    }
-                    throw new ServiceException(throwable);
-                })
-                .retryWhen(Retry.backoff(5L, Duration.ofMillis(100L))
-                        .filter(throwable -> throwable instanceof MovieException)
-                        .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> Exceptions.propagate(retrySignal.failure())))
-                .repeat()
-                .log();
-    }
-
-    public Flux<Movie> getAllMoviesRepeat(long numberOfRepeats) {
-        return movieInfoService.retrieveMoviesFlux()
-                .flatMap(this::getMovie)
-                .doOnError(throwable -> log.error("Movie error", throwable))
-                .onErrorMap(throwable -> {
-                    if (throwable instanceof NetworkException) {
-                        throw new MovieException(throwable);
-                    }
-                    throw new ServiceException(throwable);
-                })
-                .retryWhen(Retry.backoff(5L, Duration.ofMillis(100L))
-                        .filter(throwable -> throwable instanceof MovieException)
-                        .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> Exceptions.propagate(retrySignal.failure())))
-                .repeat(numberOfRepeats)
                 .log();
     }
 
